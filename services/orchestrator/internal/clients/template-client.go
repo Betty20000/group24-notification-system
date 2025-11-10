fe1fa963a9f25a9922fcabc8ac3ea831f51db594
berylatieno <berylatieno30@gmail.com>
2025-11-10 10:01:07 +0300

    feat: initiate template and user clients

---
+++ services/orchestrator/internal/clients/template-client.go
@@ -0,0 +1,10 @@
+package clients
+
+import (
+	"github.com/BerylCAtieno/group24-notification-system/services/orchestrator/internal/models"
+)
+
+type TemplateClient interface {
+	GetTemplate(templateID, language string) (*models.Template, error)
+	RenderTemplate(templateID, language string, variables map[string]interface{}) (*models.RenderResponse, error)
+}
---
+++ services/orchestrator/internal/clients/user-client.go
@@ -0,0 +1,10 @@
+package clients
+
+import (
+	"github.com/BerylCAtieno/group24-notification-system/services/orchestrator/internal/models"
+)
+
+type UserClient interface {
+	GetPreferences(userID string) (*models.UserPreferences, error)
+	GetOptOutStatus(userID string) (*models.OptOutStatus, error)
+}
